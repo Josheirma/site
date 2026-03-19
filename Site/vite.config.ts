762c963091fff98c9648c62ef7f70f3c34588203
Josheir <joshuaeirm@gmail.com>
2026-03-19 13:44:38 -0400

    vercel

--- Site/vite.config.ts
+++ Site/vite.config.ts
@@ -1,11 +1,11 @@
+//
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',               // important for Vercel hosting
   server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
+    port: 5173
+  }
 })
