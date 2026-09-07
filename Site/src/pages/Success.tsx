7e7d0ec68c46df17edc3f8cb8bf625ff2e1567da
Josheir <joshuaeirm@gmail.com>
2026-09-07 15:20:19 -0400

    License key is displayed and sent to user's email!

--- Site/src/pages/Success.tsx
+++ Site/src/pages/Success.tsx
@@ -9,17 +9,21 @@ export default function Success() {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <h1 className={styles.heading}>Payment Successful!</h1>
+        
+        
         <div className={styles.content}>
           {serial ? (
             <>
-              <p>Your license key:</p>
-              <strong>2222</strong>
+              <p>Save this license key!</p>
+              <div className={styles.licenseKey}>{serial}</div>
               <p>A copy has also been sent to your email.</p>
             </>
           ) : (
             <p>Check your email for your license key.</p>
           )}
         </div>
+
+
       </div>
     </div>
   );
